f653f1d9ab3f623e398cb49a2114a68bd9c781b4
Thuy Phan <hellothuyphan@gmail.com>
2019-04-15 01:03:19 -0700

    make cards flippable

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,23 +22,47 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+
+var resetGame = function() {
+  cardsInPlay = [];
+  var gameBoard = document.getElementById('game-board');
+  var cardElements = gameBoard.getElementsByTagName('img');
+  for (var i = 0; i < cardElements.length; i++) {
+    cardElements[i].setAttribute('src', 'images/back.png');
+  }
+};
+
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
   }
-}
+};
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   console.log("User flipped " + cards[cardId].cardImage);
   console.log("User flipped " + cards[cardId].suit);
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+  var gameBoard = document.getElementById('game-board');
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    gameBoard.appendChild(cardElement);
+  }
+  var resetButton = document.getElementById('reset');
+  resetButton.addEventListener('click', resetGame);
+};
+
+createBoard();
